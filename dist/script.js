5cd5170cf9848b9ce71c55f8f39e5c19d1e3e4e5
kenzasanaa12 <kenzasanaa00@gmail.com>
2023-05-28 18:19:35 +0000

    end project

--- dist/script.js
+++ dist/script.js
@@ -15,4 +15,26 @@ hLink.forEach(link => {
         menu.classList.toggle('hidden');
         faSolid.classList.toggle('fa-xmark');
     })
-})
+});
+
+document.addEventListener('alpine:init', () => {
+  Alpine.store('accordion', {
+    tab: 0
+  });
+  
+  Alpine.data('accordion', (idx) => ({
+    init() {
+      this.idx = idx;
+    },
+    idx: -1,
+    handleClick() {
+      this.$store.accordion.tab = this.$store.accordion.tab === this.idx ? 0 : this.idx;
+    },
+    handleRotate() {
+      return this.$store.accordion.tab === this.idx ? 'rotate-180' : '';
+    },
+    handleToggle() {
+      return this.$store.accordion.tab === this.idx ? `max-height: ${this.$refs.tab.scrollHeight}px` : '';
+    }
+  }));
+});
